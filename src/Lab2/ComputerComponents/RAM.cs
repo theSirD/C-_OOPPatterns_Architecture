4de1b23b5d2a663f183b442b5255540ca931fd8f
theSirD <isaev-daniel17@yandex.ru>
2023-10-26 15:57:25 +0300

    feat: adds one more field and check

--- src/Lab2/ComputerComponents/RAM.cs
+++ src/Lab2/ComputerComponents/RAM.cs
@@ -4,25 +4,28 @@ namespace Itmo.ObjectOrientedProgramming.Lab2.ComputerComponents;
 
 public class RAM : BaseComputerComponent
 {
-    public RAM(string name, int mmrySize, int ddrStandard, int consump)
+    public RAM(string name, int mmrySize, int ddrStandard, int consump, double freq)
     : base(name)
     {
         MemorySize = mmrySize;
         DdrStandard = ddrStandard;
         PowerConsumptionInWt = consump;
+        Frequency = freq;
     }
 
     public int MemorySize { get; private init; }
     public int DdrStandard { get; private init; }
     public int PowerConsumptionInWt { get; private init; }
+    public double Frequency { get; private init; }
     public void CanBePlaced(ComputerConfiguration computer)
     {
         if (computer?.MotherBoard is null)
             throw new ArgumentException("Install mother board first");
 
-        // if (computer.MotherBoard.ChipSet.MinMemoryFrequency > )
-            // throw new ArgumentException("This RAM is too slow");
-        if (computer.MotherBoard.SupportedDdrStandard < DdrStandard)
+        if (computer?.MotherBoard.ChipSet?.MinMemoryFrequency > Frequency)
+            throw new ArgumentException("This RAM is too slow");
+
+        if (computer?.MotherBoard.SupportedDdrStandard < DdrStandard)
             throw new ArgumentException("Mother board does not support this DDR standard");
     }
 }
