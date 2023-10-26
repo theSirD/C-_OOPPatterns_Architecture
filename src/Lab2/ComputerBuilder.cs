647cd31766f1f9cc56f79fea188ecc51eaa786e1
theSirD <isaev-daniel17@yandex.ru>
2023-10-26 16:33:48 +0300

    feat: create functionality for Build method of builder

--- src/Lab2/ComputerBuilder.cs
+++ src/Lab2/ComputerBuilder.cs
@@ -70,7 +70,18 @@ public class ComputerBuilder
 
     public void WithRam(string name)
     {
-        Computer.Ram = (RAM)_repo.Get(name);
+        var curRam = (RAM)_repo.Get(name);
+
+        try
+        {
+            curRam.CanBePlaced(Computer);
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        Computer.Ram = curRam;
     }
 
     public void WithXmp(string name)
@@ -140,6 +151,43 @@ public class ComputerBuilder
 
     public ComputerConfiguration Build()
     {
+        if (Computer.Hdd is null && Computer.Ssd is null)
+            throw new ArgumentException("Configuration does not have a storage");
+
+        if (Computer.MotherBoard is null || Computer.Cpu is null || Computer.Bios is null ||
+            Computer.Cool is null || Computer.Ram is null || Computer.PowerPack is null ||
+            Computer.ComputerCase is null)
+            throw new ArgumentException("Configuration does not have one (or more) required components");
+
+        if (!Computer.Cpu.HasGpu && Computer.DedicatedGpu is null)
+            throw new ArgumentException("Configuration does not have a GPU");
+
+        if (Computer.Cool.WidthInSm + (Computer.DedicatedGpu is null ? 0 : Computer.DedicatedGpu.WidthInSm) > Computer.ComputerCase.WidthInSm)
+            throw new ArgumentException("Components are to wide for this computer case");
+
+        if (Computer.Cool.HeightInSm + (Computer.DedicatedGpu is null ? 0 : Computer.DedicatedGpu.HeightInSm) > Computer.ComputerCase.HeightInSm)
+            throw new ArgumentException("Components are to tall for this computer case");
+
+        switch (Computer.ComputerCase.SupportedFormOfMotherBoard)
+        {
+            case MotherBoardFormFactors.MicroATX:
+                if (Computer.MotherBoard.FormFactor == MotherBoardFormFactors.ATX)
+                    throw new ArgumentException("Computer case does not support form factor of mother boards chosen");
+                break;
+
+            case MotherBoardFormFactors.MiniATX:
+                if (Computer.MotherBoard.FormFactor == MotherBoardFormFactors.ATX || Computer.MotherBoard.FormFactor == MotherBoardFormFactors.MicroATX)
+                    throw new ArgumentException("Computer case does not support form factor of mother boards chosen");
+                break;
+
+            case MotherBoardFormFactors.NanoATX:
+                if (Computer.MotherBoard.FormFactor == MotherBoardFormFactors.ATX ||
+                    Computer.MotherBoard.FormFactor == MotherBoardFormFactors.MicroATX ||
+                    Computer.MotherBoard.FormFactor == MotherBoardFormFactors.NanoATX)
+                    throw new ArgumentException("Computer case does not support form factor of mother boards chosen");
+                break;
+        }
+
         return Computer;
     }
 }
