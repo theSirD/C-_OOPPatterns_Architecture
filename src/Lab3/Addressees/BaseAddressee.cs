ecc228f9d40ee829735a38735a251e76adeed3d7
theSirD <isaev-daniel17@yandex.ru>
2023-11-11 00:39:38 +0300

    feat: adds logging to BaseAddressee

--- src/Lab3/Addressees/BaseAddressee.cs
+++ src/Lab3/Addressees/BaseAddressee.cs
@@ -1,11 +1,13 @@
 using System;
 using Itmo.ObjectOrientedProgramming.Lab3.Enums;
+using Itmo.ObjectOrientedProgramming.Lab3.Services;
 using Itmo.ObjectOrientedProgramming.Lab3.Targets;
 
 namespace Itmo.ObjectOrientedProgramming.Lab3.Addressees;
 
 public abstract class BaseAddressee
 {
+    private readonly ILogger _logger = new Logger();
     private Message? _currentMessage;
     protected BaseAddressee(ConfidentialityLevels confLevelAccess)
     {
@@ -25,6 +27,7 @@ public abstract class BaseAddressee
             if (value.ConfidentialityLevel > ConfidentialityLevelAccess)
                 throw new ArgumentException("This addressee does not have right to read this message");
             _currentMessage = value;
+            _logger.LogOneMessage($"Addressee with a confidence level of {ConfidentialityLevelAccess} got a message");
         }
     }
 
