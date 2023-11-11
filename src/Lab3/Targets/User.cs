d3bd20205ee9da7778bed283df0025ca121636c0
theSirD <isaev-daniel17@yandex.ru>
2023-11-11 10:31:37 +0300

    feat: add suplementary methods

--- src/Lab3/Targets/User.cs
+++ src/Lab3/Targets/User.cs
@@ -7,6 +7,13 @@ public class User : IRecieve
     private List<Message> _unreadMessages = new();
     private List<Message> _readMessages = new();
 
+    public User(string name)
+    {
+        Name = name;
+    }
+
+    public string? Name { get; private set; }
+
     public void RecieveMessage(Message message)
     {
         _unreadMessages.Add(message);
@@ -16,5 +23,25 @@ public class User : IRecieve
     {
         _readMessages.Add(_unreadMessages[numberOfMessage]);
         _unreadMessages.RemoveAt(numberOfMessage); // Add catch
+    }
+
+    public bool CheckIfMessageIsUnread(string header)
+    {
+        foreach (Message message in _unreadMessages)
+        {
+            if (message.Header == header) return true;
+        }
+
+        return false;
+    }
+
+    public bool CheckIfMessageIsRead(string header)
+    {
+        foreach (Message message in _readMessages)
+        {
+            if (message.Header == header) return true;
+        }
+
+        return false;
     }
 }
