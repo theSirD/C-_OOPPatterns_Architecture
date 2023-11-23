6f63ef28a14de6edfe3ba22ea16720be9379dcec
theSirD <isaev-daniel17@yandex.ru>
2023-11-23 16:56:02 +0300

    feat: create disconnection command handler

---
+++ src/Lab4/ConsoleCommandHandlers/DisconnectHandler.cs
@@ -0,0 +1,34 @@
+using System;
+using Itmo.ObjectOrientedProgramming.Lab4.ConsoleCommandHandlers.TreeHandlers;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.ConsoleCommandHandlers;
+
+public class DisconnectHandler : BaseHandler
+{
+    public DisconnectHandler()
+    {
+        NextHandler = new TreeGoToHandler();
+    }
+
+    public override void Handle(string request, string path)
+    {
+        if (CanHandle(request))
+        {
+            if (FileSystem is null)
+                throw new ArgumentException("You are not connected to FS yet");
+            FileSystem.Disconnect();
+        }
+        else
+        {
+            if (NextHandler is null)
+                Console.WriteLine("Can not do the command");
+            else
+                NextHandler.Handle(request, string.Empty);
+        }
+    }
+
+    public override bool CanHandle(string request)
+    {
+        return request == "disconnect";
+    }
+}
