c9e499a7873a976ad5a89b5ff22b76b056f89b57
theSirD <isaev-daniel17@yandex.ru>
2023-11-25 00:16:27 +0300

    refactor: change tree list command's handler

--- src/Lab4/ConsoleCommandHandlers/TreeHandlers/TreeListHandler/SelectionDepthFlagHandler.cs
+++ src/Lab4/ConsoleCommandHandlers/TreeHandlers/TreeListHandler/SelectionDepthFlagHandler.cs
@@ -6,6 +6,8 @@ public class SelectionDepthFlagHandler : BaseHandler
 {
     public override void Handle(string request, string path)
     {
+        if (Parser is null)
+            throw new ArgumentException("You need to pass a parser first");
         if (!CanHandle(request))
         {
             if (NextHandler is null)
@@ -13,15 +15,13 @@ public class SelectionDepthFlagHandler : BaseHandler
             NextHandler.Handle(request, string.Empty);
         }
 
-        string? flagArgument = Parser?.SearchForFlag();
-        if (flagArgument is not null)
-        {
-            if (flagArgument.Length == 0)
-                throw new ArgumentException("Flag argument is not specified after flag");
-            if (FileSystem is null)
-                throw new ArgumentException("You need to connect to FS first");
-            FileSystem.TreeList(flagArgument);
-        }
+        Parser.MoveForward();
+        string flagArgument = Parser.Current;
+        if (flagArgument.Length == 0)
+            throw new ArgumentException("Flag argument is not specified after flag");
+        if (FileSystem is null)
+            throw new ArgumentException("You need to connect to FS first");
+        FileSystem.TreeList(flagArgument);
     }
 
     public override bool CanHandle(string request)
